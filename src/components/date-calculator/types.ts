d14abbdae675fca82482583bc723c7195011a179
AllenJame123 <allenjame1234@.com>
2025-06-02 17:51:30 +0500

    chore: remove src directory for minimal Remix + Netlify repro

--- src/components/date-calculator/types.ts
+++
@@ -1,8 +0,0 @@
-export type CalculationType = "difference" | "add" | "subtract" | "business";
-
-export interface DateResult {
-  totalDays: number;
-  businessDays: number;
-  formattedDate?: string;
-  dayOfWeek?: string;
-}
